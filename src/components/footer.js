c1aa7e8a9517fec2bebcbb52698941d495386693
Joakoq12 <fullestnewyork@gmail.com>
2023-03-14 23:06:39 -0400

    instalacion de tailwind

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+  return (
+    
+    <footer className='text-red-500'>
+        <small>Developed and created by Joaquin Martinez. ©2023. All the rights reserved.</small>
+    </footer>
+
+  );
+}
+
+export default Footer;
---
+++ src/components/principal.js
@@ -0,0 +1,20 @@
+
+
+function Principal({children}) {
+  return (
+<>
+ <sidenav>
+    <a href='/lista'>Lista</a>
+    <a href='/crear'>Crear</a>
+  
+ </sidenav>
+ <main>
+    {children}
+ </main>
+</>
+
+
+  );
+}
+
+export default Principal;
