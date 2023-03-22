7df26c8ac4e7fb4930cb46bda2fa1bf1c0707ed1
Joakoq12 <fullestnewyork@gmail.com>
2023-03-21 23:06:50 -0400

    Primera fase completa

--- src/components/compartidos/principal.js
+++ src/components/compartidos/principal.js
@@ -1,5 +1,5 @@
 
-import './Principal.css';
+import estilos from './Principal.module.css';
 import Vinculo from './vinculo';
 import { ReactComponent as Lista } from '../img/lista.svg';
 import { ReactComponent as Nueva } from '../img/nueva.svg';
@@ -8,22 +8,20 @@ import { ReactComponent as Nueva } from '../img/nueva.svg';
 function Principal({children}) {
   return (
 
-  <div className='principal'>
- <aside className='aside'>
+  <div className={estilos.principal}>
+ <aside className={estilos.aside}> 
 
-    <Vinculo href='/lista' text='Lista de Metas'>
-      <Lista className='icono'></Lista>
-        
-
-    </Vinculo>
-    <Vinculo href='/crear' text='Nuevas Metas'>
-      <Nueva className='icono'></Nueva>
-
-    </Vinculo>
+    <Vinculo 
+    href='/lista' 
+    text='Lista de Metas'
+    Icono={Lista} />
+      
+    <Vinculo 
+    href='/crear' 
+    text='Nuevas Metas'
+    Icono={Nueva}
     
-    
-
- 
+    />
   
  </aside>
  <main className='main'>
